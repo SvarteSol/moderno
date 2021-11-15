82437e521e06971fa4c97bec457b8af46191f1d4
SvarteSol <sur14tur88@gmail.com>
2021-11-15 16:34:29 +0200

    added product-slider product

--- app/dest/scripts/script.js
+++ app/dest/scripts/script.js
@@ -11,4 +11,34 @@ function closeDialog() {
 
 $(function(){
 
+    $(function () {
+ 
+        $(".rate-star").rateYo({
+          rating: 5,
+          readOnly: true,
+          starWidth: "12px"
+        });
+      }); 
+
+        // $(".product-slider__inner").slick({
+        //     dots: true,
+        //     slidesToShow: 1,
+        //     slidesToScroll: 4,
+        //     arrows: false,
+        //     centerMode: true,
+        //     draggable: true,
+
+        // });
+
+
+
+
+
+
+
+
+
+
+    var mixer = mixitup('.products__inner-box');
 });
+
